569baaba034841fe070ac6ab73a28eea8b0c9b43
oltimaloku <oltimaloku555@gmail.com>
2024-01-09 17:12:57 -0800

    Added gallery UI with grid and clickable photos

--- flutter_client/lib/features/gallery/views/geo_sphere_gallery.dart
+++ flutter_client/lib/features/gallery/views/geo_sphere_gallery.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:georeal/features/gallery/widgets/gallery_navbar.dart';
 import 'package:georeal/models/geo_sphere_model.dart';
 import 'package:provider/provider.dart';
 
-import '../../geo_sphere/widgets/gallery_navbar.dart';
-import '../services/gallery_service.dart'; // Import GalleryService
+import '../services/gallery_service.dart';
+
+// Import GalleryService
 
 class GeoSphereGallery extends StatelessWidget {
   final GeoSphere geoSphere;
@@ -19,6 +21,12 @@ class GeoSphereGallery extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Dummy photo path (replace with an actual path of a local image)
+    //const String dummyPhotoPath = 'assets/images/profile_photo.jpg';
+
+    // Create a list of 9 dummy photo paths
+    // List<String> photoPaths = List.filled(9, dummyPhotoPath);
+
     final galleryService = Provider.of<GalleryService>(context, listen: false);
     List<String> photoPaths = galleryService
         .getPhotosFromGallery(geoSphere.galleryId); // Fetch photo paths
@@ -32,19 +40,48 @@ class GeoSphereGallery extends StatelessWidget {
             ),
             Expanded(
               // Wrap ListView.builder with Expanded
-              child: ListView.builder(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 0,
+                  mainAxisSpacing: 6,
+                ),
                 itemCount: photoPaths.length,
                 itemBuilder: (context, index) {
                   String photoPath = photoPaths[index];
-                  return Image.file(
-                    File(photoPath),
-                  ); // Display the image (assuming these are network images)
+                  return GestureDetector(
+                    child: Image.file(
+                      File(photoPath),
+                    ),
+                    onTap: () => _showFullSizeImage(context, photoPath),
+                  );
+                  /*Image.asset(
+                          photoPath); */ // Display the image (assuming these are network images)
                 },
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  _showFullSizeImage(BuildContext context, String photoPath) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Image.file(File(photoPath)),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
