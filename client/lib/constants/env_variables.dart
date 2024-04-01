8e219bfabcf09c9fef632c5af339d03737e393d6
oltimaloku <oltimaloku555@gmail.com>
2024-03-31 19:21:04 -0700

    changed address to accept all devices on local network to allow external emulator to connect to backend

--- client/lib/constants/env_variables.dart
+++ client/lib/constants/env_variables.dart
@@ -3,5 +3,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class EnvVariables {
   static String googleApiKey = dotenv.env['GOOGLE_API_KEY']!;
   // static String uri = dotenv.env['SERVER_URI']!;
-  static String uri = 'http://localhost:5000';
+  static String uri = 'http://${dotenv.env['IP_ADDRESS']}:8000';
 }
